4ca1e4722a8ccc649dfcccf62a1d53d586b7ed57
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-16 13:23:39 +0800

    Finalized ideas for password generator!

--- password_generator.py
+++ password_generator.py
@@ -2,14 +2,19 @@
 # Password generator ideas: 
 """
 #! Have the password specifications for each function above, eg. random 10 letter passwords
-- Function to chose parameters eg. how many numbers, how many letters etc..
-- Nested function to generate multiple passwords
-- Give a password and add things to it so it meats certain requirements eg. # of special characters..
+? Maybe have a password generator class and then diff generation functions, so you pick which one you want to use..
+
+? Diff function ideas: (4 should be plenty!)
+- Random combo for n length
+- Random generation where you chose the total length and how many numbers and characters you want in it?
+- Give a password and it converts it a certain format to at least include 1 upper character and 1 punct symbol:
+    eg. input mango output: Mango6
+- Generate a list of n passwords to pick from
+
 """
 
 import string
 import random
-
 
 lower_alphabet = string.ascii_lowercase
 upper_alphabet = string.ascii_uppercase
@@ -19,6 +24,7 @@ digits = string.digits
 punctuation = string.punctuation
 
 
-# Description... (undecided)
-def make_simple_password(x):
+# Password generator 1: Generates password of length n
+def make_simple_password(length):
     password = ""
+
