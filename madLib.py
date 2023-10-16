97c14b404fcc461f1034504a92594ed99d785b9b
thePangster99 <isepang@gmail.com>
2023-10-16 11:19:35 +0800

    Update madLib.py

--- madLib.py
+++ madLib.py
@@ -3,13 +3,13 @@
 #! Taking user inputs 
 name = input("Enter name: ") 
 school = input("What school did you go to? ")
-cake = input("On a scale of 1-10 how much do you like cake? ")
+food = input("On a scale of 1-10 how much do you like food? ")
 sport = input("What sport do you play? ")
 ability = input("Were you any good? ")
 
 #* Creating the Story
 awesome_story = f"There once was a boy named {name}. He went to {school} during his school years.
-When it comes to how much he likes cake on a scale of 1-10 he says {cake}. He played {sport} and 
+When it comes to how much he likes cake on a scale of 1-10 he says {food}. He played {sport} and 
 answered {ability} when it came to describing how good he was."
 
 #?Printing the story 
