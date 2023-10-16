71b95ca4249488cef13c33028502d35b861cdb5a
thePangster99 <isepang@gmail.com>
2023-10-16 11:11:47 +0800

    Create madLib.py

---
+++ madLib.py
@@ -0,0 +1,17 @@
+# madLib game
+
+#! Taking user inputs 
+name = input("Enter name: ") 
+school = input("What school did you go to? ")
+gimput = input("On a scale of 1-10 how much of a gimp were you in school? ")
+sport = input("What sport do you play? ")
+ability = input("Were you any good? ")
+
+#* Creating the Story
+awesome_story = f"There once was a boy named {name}. He went to {school} during his school years.
+When it comes to rating how much of a gimp he was he rates himself a {gimput}. He played {sport} and 
+answered {ability} when it came to describing how good he was."
+
+#?Printing the story 
+print(awesome_story)
+
