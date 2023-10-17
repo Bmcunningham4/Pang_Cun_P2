7186d8362c7d5851dc2044ad3ff7cfe4bcbf8650
thePangster99 <isepang@gmail.com>
2023-10-17 10:52:02 +0800

    Update madLib.py

--- madLib.py
+++ madLib.py
@@ -7,10 +7,11 @@ food = input("On a scale of 1-10 how much do you like food? ")
 sport = input("What sport do you play? ")
 ability = input("Were you any good? ")
 
+#! Converting food to integer
+food = int(food)
+
 #* Creating the Story
-awesome_story = f"There once was a boy named {name}. He went to {school} during his school years.
-When it comes to how much he likes cake on a scale of 1-10 he says {food}. He played {sport} and 
-answered {ability} when it came to describing how good he was."
+awesome_story = f"There once was a boy named {name}. He went to {school} during his school years. When it comes to how much he likes food on a scale of 1-10 he says {food}. He played {sport} and answered {ability} when it came to describing how good he was."
 
 #?Printing the story 
 print(awesome_story)
