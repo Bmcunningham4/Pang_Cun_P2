37940ddc62305f39eb22c1a4cade1ee21aa663cc
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-19 10:08:58 +0800

    Ideas finalised for flashcards!!!

--- Cun's projects/flashcard_quiz.py
+++ Cun's projects/flashcard_quiz.py
@@ -1,3 +1,15 @@
-# Make a start on this you big legend
+# Key features/ ideas that my glashcard quiz game must have...
+"""
+? Need
+- User interface 
+- scoring system
+- randomization so it's not boring in same order every time..
+- end of game summary
+? optional
+- timer (probs not)
+- question answer storage (probs only needed if I was making it a big thing..)
+- ability to make flashcards instead of just being quizzed but probs too complicated..
+"""
 
-# todo: and read above ^^^ !!!
+import random
+
