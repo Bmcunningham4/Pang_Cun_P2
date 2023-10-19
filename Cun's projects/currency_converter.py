94e275cfe454077352d2a0b4274c6070d3a75ab5
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-19 18:26:04 +0800

    Great ideas out there for my currency converter...
    Your gonna kill it go get em king...

---
+++ Cun's projects/currency_converter.py
@@ -0,0 +1,17 @@
+#Key features/ required components:
+"""
+? Essential:
+- Connected to live currencies
+- Can pick which currency you want to convert and the amount
+- Need a function to fetch specific currencies
+- And then a diff function to do the maths conversion!...
+
+? Optional:
+- If I can't figure out how to connect to API, then use monopoly money and cun bucks as examples as well as some real googled ones...
+
+? Apaz best to fetch live rates from one of these API's..
+- Open Exchange Rates
+- Fixer.io
+- CurrencyLayer
+- ExchangeRatesAPI.io
+"""
--- test_file.py
+++ test_file.py
@@ -1,1 +1,42 @@
 
+#! Example currency converter!! so I can understand how it works..
+
+import requests
+
+# Define your API key and the API URL
+api_key = 'YOUR_API_KEY'  # Replace with your actual API key
+api_url = 'https://api.apilayer.com/exchangerates_data/latest'
+
+# Function to fetch exchange rates
+def get_exchange_rates(base_currency='USD'):
+    params = {'base': base_currency, 'apikey': api_key}
+    response = requests.get(api_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['rates']
+    else:
+        print('Failed to fetch exchange rates.')
+        return None
+
+# Function to convert currency
+def convert_currency(amount, from_currency, to_currency):
+    rates = get_exchange_rates(from_currency)
+    if rates:
+        if to_currency in rates:
+            converted_amount = amount * rates[to_currency]
+            return converted_amount
+        else:
+            print(f"Conversion from {from_currency} to {to_currency} is not supported.")
+    return None
+
+# Main program
+if __name__ == '__main__':
+    from_currency = 'USD'
+    to_currency = 'EUR'
+    amount = 100
+
+    converted_amount = convert_currency(amount, from_currency, to_currency)
+    if converted_amount:
+        print(f"{amount} {from_currency} is equal to {converted_amount:.2f} {to_currency}")
+
