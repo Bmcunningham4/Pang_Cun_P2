6ea00cd3b0889c5fd79436384ae2c6b96703b47b
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-19 14:45:28 +0800

    Added my saucy q6 lolll

--- Cun's projects/flashcard_quiz.py
+++ Cun's projects/flashcard_quiz.py
@@ -16,3 +16,49 @@
 """
 
 import random
+import sys
+
+#! Need to get better with this shite...
+file_path = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects/quiz_qna.py" # raw string bby to account for space and '
+sys.path.append(file_path)
+
+# Ohhhhhh The reason you do this is because then you don't have to use the file name each time..
+from quiz_qna import (
+    question_1,
+    question_2,
+    question_3,
+    question_4,
+    question_5
+)
+
+print("Welcome to my lit NBA themed Quiz... Yew")
+
+list_of_questions = [question_1, question_2, question_3, question_4, question_5]
+random.shuffle(list_of_questions) # You don't assign this to a variable just do it in place...
+
+
+def display_questions():
+    q_number = 0
+    num_correct = 0 
+    
+    for question in list_of_questions:
+        q_number += 1
+        print()
+        print(f"Question {q_number}:")
+        print(question["question"])
+        options = question["options"]
+
+        num = 0
+        for option in options:
+            letter = ["A", "B", "C"]
+            print(letter[num], option)
+            num += 1
+
+        print()
+        user_answer = input("Enter: A, B or C: ").upper()
+
+display_questions()
+
+# print(question_1["correct_answer"])
+
+
--- Cun's projects/quiz_qna.py
+++ Cun's projects/quiz_qna.py
@@ -1,34 +1,43 @@
-simple_question1 = {
+# I had to change all these to have the Multichoice letter answer instead of the actual answer..
+
+question_1 = {
     "question": "Which NBA team has won the most championships?",
     "options": ["Boston Celtics", "Golden State Warriors", "Los Angeles Lakers"],
-    "correct_answer": "Boston Celtics",
+    "correct_answer": "A",
     "explanation": "The Boston Celtics have won 17 NBA championships, more than any other team."
 }
 # Lol
-simple_question2 = {
+question_2 = {
     "question": "Which NBA player is the all-time leading scorer?",
     "options": ["LeBron James", "Kareem Abdul-Jabbar", "Michael Jordan"],
-    "correct_answer": "Kareem Abdul-Jabbar",
+    "correct_answer": "B",
     "explanation": "Kareem Abdul-Jabbar scored 38,387 points in his NBA career, more than any other player."
 }
 
-simple_question3 = {
+question_3 = {
     "question": "Which NBA player has won the most MVP awards?",
     "options": ["LeBron James", "Michael Jordan", "Kareem Abdul-Jabbar"],
-    "correct_answer": "LeBron James",
+    "correct_answer": "A",
     "explanation": "LeBron James has won 4 NBA MVP awards, more than any other player."
 }
 
-simple_question4 = {
+question_4 = {
     "question": "Which NBA player is the all-time leading rebounder?",
     "options": ["Wilt Chamberlain", "Kareem Abdul-Jabbar", "Bill Russell"],
-    "correct_answer": "Wilt Chamberlain",
+    "correct_answer": "A",
     "explanation": "Wilt Chamberlain grabbed 23,924 rebounds in his NBA career, more than any other player."
 }
 
-simple_question5 = {
+question_5 = {
     "question": "Which NBA team has the longest winning streak?",
     "options": ["Los Angeles Lakers", "Golden State Warriors", "Milwaukee Bucks"],
-    "correct_answer": "Los Angeles Lakers",
+    "correct_answer": "A",
     "explanation": "The Los Angeles Lakers won 33 games in a row in the 1971-72 season, the longest winning streak in NBA history."
+}
+
+question_6 = {
+    "question": "Who is the only player to beat Chef Curry in a 3 pt cook out?",
+    "options": ["Steph Curry's mom", "Chef Cunningham", "Ben Cunningham"],
+    "correct_answer": "C",
+    "explanation": "Famously back in 2018 Ben Cunningham received a wildcard entrance to the 3pt chef off, and won..."
 }
