4f8c756b81261dd8f0f27e9cc1609822f8cd173c
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-19 10:41:56 +0800

    Made a new file to store q_a's !!!

--- Cun's projects/flashcard_quiz.py
+++ Cun's projects/flashcard_quiz.py
@@ -9,7 +9,10 @@
 - timer (probs not)
 - question answer storage (probs only needed if I was making it a big thing..)
 - ability to make flashcards instead of just being quizzed but probs too complicated..
+- Ability to chose what quiz game you want to play eg. have diff ones!..
+
+? Note:
+- I think it would be easier if I had the answers just in variables downn the page but its better dictionary practice if I have them stored in dicts..
 """
 
 import random
-
---
+++ Cun's projects/quiz_q&a_storage.py
@@ -0,0 +1,34 @@
+simple_question1 = {
+    "question": "Which NBA team has won the most championships?",
+    "options": ["Boston Celtics", "Golden State Warriors", "Los Angeles Lakers"],
+    "correct_answer": "Boston Celtics",
+    "explanation": "The Boston Celtics have won 17 NBA championships, more than any other team."
+}
+# Lol
+simple_question2 = {
+    "question": "Which NBA player is the all-time leading scorer?",
+    "options": ["LeBron James", "Kareem Abdul-Jabbar", "Michael Jordan"],
+    "correct_answer": "Kareem Abdul-Jabbar",
+    "explanation": "Kareem Abdul-Jabbar scored 38,387 points in his NBA career, more than any other player."
+}
+
+simple_question3 = {
+    "question": "Which NBA player has won the most MVP awards?",
+    "options": ["LeBron James", "Michael Jordan", "Kareem Abdul-Jabbar"],
+    "correct_answer": "LeBron James",
+    "explanation": "LeBron James has won 4 NBA MVP awards, more than any other player."
+}
+
+simple_question4 = {
+    "question": "Which NBA player is the all-time leading rebounder?",
+    "options": ["Wilt Chamberlain", "Kareem Abdul-Jabbar", "Bill Russell"],
+    "correct_answer": "Wilt Chamberlain",
+    "explanation": "Wilt Chamberlain grabbed 23,924 rebounds in his NBA career, more than any other player."
+}
+
+simple_question5 = {
+    "question": "Which NBA team has the longest winning streak?",
+    "options": ["Los Angeles Lakers", "Golden State Warriors", "Milwaukee Bucks"],
+    "correct_answer": "Los Angeles Lakers",
+    "explanation": "The Los Angeles Lakers won 33 games in a row in the 1971-72 season, the longest winning streak in NBA history."
+}
