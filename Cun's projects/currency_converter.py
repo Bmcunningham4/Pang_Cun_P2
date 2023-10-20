3b6a7a5872a94ec0868ce844ed66150eded29e16
Ben Cunningham <bmcunningham4@outlook.com>
2023-10-20 12:07:02 +0800

    Updated currency converter plan..

--- Cun's projects/currency_converter.py
+++ Cun's projects/currency_converter.py
@@ -4,7 +4,7 @@
 - Connected to live currencies
 - Can pick which currency you want to convert and the amount
 - Need a function to fetch specific currencies
-- And then a diff function to do the maths conversion!...
+- And then a diff function to do the maths conversion!...\
 
 ? Optional:
 - If I can't figure out how to connect to API, then use monopoly money and cun bucks as examples as well as some real googled ones...
