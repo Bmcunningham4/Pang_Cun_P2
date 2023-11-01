ecb84136033276746a20df087353f7686a06fcf5
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-01 10:31:50 +0800

    Simplified is better
    Lol the print message..

--- home_page.py
+++ home_page.py
@@ -16,28 +16,13 @@
 import sys
 
 #? Get all my file paths!
-# Cun's
-file_path1 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects/password_generator.py"
-file_path2 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects/ATM.py"
-file_path3 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects/quiz.py"
-file_path4 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects/currency_converter.py"
-
-# Pang's  #! Note to Pang: Interested to see if these work for you becausse you might have a diff file path on you computer to me..
-file_patha = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Pang's projects/calculator.py"
-file_pathb = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Pang's projects/madLib.py"
-file_pathc = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Pang's projects/numGuess.py"
-file_pathd = r""
+file_path1 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects"
+file_path2 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Pang's projects"
 
 #? Upload all filepaths...
 sys.path.append(file_path1)
 sys.path.append(file_path2)
-sys.path.append(file_path3)
-sys.path.append(file_path4)
 
-sys.path.append(file_patha)
-sys.path.append(file_pathb)
-sys.path.append(file_pathc)
-sys.path.append(file_pathd)
 
 #? Import all files
 import password_generator
