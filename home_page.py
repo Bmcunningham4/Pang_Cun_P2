e565c806be05c4b65d7a60be47f0f03e9c21a443
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-07 10:54:36 +0800

    Starting to tidy up whole thing...

--- home_page.py
+++ home_page.py
@@ -1,30 +1,8 @@
-
-#? Welcome to the App Oasis ahahhaahha
-# Othe ideas for names.. 
-""""
-"App Switchboard"
-"App Launcher"
-"App Plaza"
-"App Center"
-"App Junction"
-"App Navigator"
-"App Hub Central"
-"App Carousel"
-"App Select"
-"""
-
 import sys
 
 #? Get all my file paths!
 file_path1 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Cun's projects"
 file_path2 = r"/Users/bencunningham/Documents/GitHub/Pang_Cun_P2/Pang's projects"
-
-"""
-# Trying relative paths..
-file_path1 = r"/GitHub/Pang_Cun_P2/Cun's projects"
-file_path2 = r"/GitHub/Pang_Cun_P2/Pang's projects"
-"""
- 
 
 #? Upload all filepaths...
 sys.path.append(file_path1)
