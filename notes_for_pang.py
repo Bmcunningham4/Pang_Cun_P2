e846c5a383bbbab4bfff632437dec0f36fd1492d
Ben Cunningham <bmcunningham4@outlook.com>
2023-11-07 14:22:04 +0800

    Cleaned notes for pang

--- notes_for_pang.py
+++ notes_for_pang.py
@@ -1,25 +1,25 @@
 
 #? Changes that I made to pang's
 """
+- Added an __init__.py file to your folder, which allows for relative path use
+
 Calculator:
 - Intro message
-- Added spaces in between each input, slighlty visually nicer..
-- calculator causes error when you try use a string so fixed that... (added get_int.. function..)
+- Added spaces in between each input
+- calculator causes error when you try type a string so fixed that... (added get_int.. function..)
 
 MadLib:
 - Intro message
 - Error proofed the integer input for food
-- Changed story so wasn't on 1 line using """ """
+- Changed story so wasn't on 1 line, using """ """
 
 numGuess:
 - Intro message
-- Changed function to main for continuity
+- Changed function name to main for continuity
 - Spacing (soz if you don't like..)
 
 """
 
-#? Final things that will need fixing when in 1 file at the end..
-"""
-- Make sure that the get_integer_input function can be used without sys append crap...
-- The files don't work on there individual files but work overall...
-"""
+#? Final notes"
+# My files don't seem to work on the files themselves but all seem to work from homepage! 
+# Has something to do with my get_integer_input function, which I'm importing all over the place..
